a315ed2f64b3231fc86555f8da12a5819d63c7eb
Dmitry <60636938+aj4kaa@users.noreply.github.com>
2020-04-09 18:22:19 +0300

    modal1

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,21 @@ jQuery(document).ready(function () {
 		slider1.trigger('prev.owl.carousel');
 	});
 
+	//MODAL
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow')
+		});
+	});
+
 	// Form
 
 	// let message = {
