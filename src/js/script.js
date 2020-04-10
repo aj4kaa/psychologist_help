e328f1a7866c469df2973d188d6f5ee573aba320
Dmitry <60636938+aj4kaa@users.noreply.github.com>
2020-04-10 16:13:34 +0300

    desk beta 1.0

--- src/js/script.js
+++ src/js/script.js
@@ -101,6 +101,37 @@ jQuery(document).ready(function () {
 		});
 	});
 
+	// SEND FORM
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	// Smooth scroll adn pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else $('.pageup').fadeOut();
+	});
+
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
 	// Form
 
 	// let message = {
